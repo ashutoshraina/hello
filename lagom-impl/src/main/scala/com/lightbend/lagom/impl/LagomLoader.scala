098886bd62c8dfd0c14a4767bf2dd51a895b0bbe
Felix Satyaputra <felix.satyaputra@typesafe.com>
2017-08-22 18:38:44 +1000

    Trying to setup Dns Service Locator using KubernetesRuntimeComponents

--- lagom-impl/src/main/scala/com/lightbend/lagom/impl/LagomLoader.scala
+++ lagom-impl/src/main/scala/com/lightbend/lagom/impl/LagomLoader.scala
@@ -1,25 +1,32 @@
 package com.lightbend.lagom.impl
 
-import com.lightbend.external.api.{LagomService, LibertyService}
-import com.lightbend.lagom.scaladsl.api.ServiceLocator
-import com.lightbend.lagom.dns
-import com.lightbend.lagom.dns.DnsServiceLocator
+import com.lightbend.external.api.{LibertyService, LagomService}
+import com.lightbend.lagom.scaladsl.client.LagomServiceClientComponents
 import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
 import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext, LagomApplicationLoader, LagomServer}
+import com.lightbend.lagom.dns.DnsServiceLocator
+import com.lightbend.dns.locator.{ ServiceLocator => ServiceLocatorService }
 import com.softwaremill.macwire.wire
 import play.api.libs.ws.ahc.AhcWSComponents
 
 class LagomLoader extends LagomApplicationLoader{
-
   override def load(context: LagomApplicationContext): LagomApplication =
-    new MyLagomApplication(context) {
-      override def serviceLocator: ServiceLocator = new DnsServiceLocator()
-    }
+    new MyLagomApplication(context) with KubernetesRuntimeComponents
 
   override def loadDevMode(context: LagomApplicationContext): LagomApplication =
     new MyLagomApplication(context) with LagomDevModeComponents
 
   override def describeService = Some(readDescriptor[LibertyService])
+}
+
+sealed trait KubernetesRuntimeComponents extends LagomServiceClientComponents {
+  val serviceLocatorService = actorSystem.actorOf(ServiceLocatorService.props, ServiceLocatorService.Name)
+  lazy val serviceLocator =
+    new DnsServiceLocator(
+      serviceLocatorService,
+      this.actorSystem,
+      this.actorSystem.dispatcher
+    )
 }
 
 abstract class MyLagomApplication(context: LagomApplicationContext) extends LagomApplication(context)
@@ -28,5 +35,4 @@ abstract class MyLagomApplication(context: LagomApplicationContext) extends Lago
   // Bind the service that this server provides
   override lazy val lagomServer: LagomServer = serverFor[LagomService](wire[LagomServiceImpl])
   lazy val libertyService = serviceClient.implement[LibertyService]
-
 }
--- lagom-impl/src/main/scala/com/lightbend/lagom/impl/LagomServiceImpl.scala
+++ lagom-impl/src/main/scala/com/lightbend/lagom/impl/LagomServiceImpl.scala
@@ -10,6 +10,15 @@ import scala.concurrent.ExecutionContext
 class LagomServiceImpl(externalService: LibertyService, serviceLocator: ServiceLocator)(implicit ec: ExecutionContext) extends LagomService {
 
   override def speak(): ServiceCall[NotUsed, String] = ServiceCall { _ =>
+    // TODO: if the echo() doesn't work, uncomment below, and see if the service locator can resolve the service using
+    // its service name or raw SRV entry.
+    // Also confirm that serviceLocator is an instance of DnsServiceLocator.
+    //val libertyServiceName = "libertyservice"
+    //serviceLocator.locate(libertyServiceName ).map(v => s"SRV $libertyServiceSrvName: got $v")
+
+    //val libertyServiceSrvName = "liberty service SRV value record goes here"
+    //serviceLocator.locate(libertyServiceSrvName).map(v => s"SRV $libertyServiceSrvName: got $v")
+
     externalService.echo().invoke().map(result => s"External Service Replied with --- \n$result")
   }
 
